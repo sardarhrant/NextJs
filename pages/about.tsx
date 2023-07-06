63e9df28dc3ebe7b4fd05666fdae7f9c49441065
Hrant Sardaryan <hrant_sardaryan@epam.com>
2023-07-06 17:27:37 +0400

    Linking and Navigation

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,20 @@
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
 export default function AboutPage() {
-  return "About";
+  const router = useRouter();
+  const goBack = useCallback(() => {
+    router.back();
+  }, []);
+
+  const goHome = useCallback(() => {
+    router.push('/');
+  }, []);
+
+  return (
+    <>
+      <button onClick={goBack}>Go back</button>
+      <button onClick={goHome}>Go Home</button>
+    </>
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,12 @@
-import { useState } from "react";
+import React from "react";
+import Link from "next/link";
 
-export default function IndexPage() {
-  const [word, setWord] = useState('');
-  const [transformed, setTransformed] = useState('');
-
-  const transform = type => {
-    fetch(`/api/transform/${type}/?word=${word}`)
-      .then(res => res.json())
-      .then(({ error, result }) => {
-        if (error) setTransformed(error);
-        setTransformed(result);
-      });
-  };
-
-  const transformLower = () => transform('lower');
-  const transformUpper = () => transform('upper');
+export default function Index() {
   return (
-    <div>
-      <input type="text" onChange={e => setWord(e.target.value)} value={word} />
-      <button onClick={transformLower}>Transform Lower</button>
-      <button onClick={transformUpper}>Transform Upper</button>
-      <h2>Result transformed: {transformed}</h2>
-    </div>
+    <>
+      <Link href="/about">
+        About
+      </Link>
+    </>
   );
-}
+};
