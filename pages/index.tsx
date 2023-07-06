eb7d6b219d9ad5daac0d4a5b6b528d8ad5d327d2
Hrant Sardaryan <hrant_sardaryan@epam.com>
2023-07-06 17:50:54 +0400

    Styling approaches

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,36 @@
-import React from "react";
-import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import styles from './index.module.css';
 
 export default function Index() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(time + 1);
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [time]);
+
+
   return (
     <>
-      <Link href="/about">
-        About
-      </Link>
+      <div className={styles.timer}>
+        <p style={{ color: "violet" }}>Time:</p>
+        <span>{time}</span>
+      </div>
+
+      <style jsx>{`
+      p {
+        font-weight: bold;
+      }
+
+      span {
+        color: red;
+      }
+      `}</style>
     </>
   );
 };
